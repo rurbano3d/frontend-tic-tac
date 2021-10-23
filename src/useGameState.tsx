c08621835923ffa65d228f8c17623d5b6847b285
Rodrigo Urbano <ext.rodrigo.urbano@gmail.com>
2021-10-23 16:27:54 -0300

    fix bug that not ensure the correct working of game

--- src/useGameState.tsx
+++ src/useGameState.tsx
@@ -6,17 +6,19 @@ import { useState } from "react";
 
 type Player = 'X' | 'O'
 
-let nextPlayer: Player = 'X';
 let currentBoard = Array(9).fill(null);
 const useGameState = () => {
   const [stepNumber, setStepNumber] = useState(0);
-
+  const [nextPlayer, setNextPlayer] = useState<Player>('X');
   const computeMove = (nextPlayer: Player, squareId: any) => {
     if (nextPlayer === 'X') {
-      nextPlayer = 'O'
+      currentBoard[squareId] = "❌";
+      setNextPlayer('O');
     } else {
-      nextPlayer = 'X'
+      currentBoard[squareId] = "⭕";
+      setNextPlayer('X');
     }
+    
     setStepNumber((currentStepNumber) => currentStepNumber + 1);
   }
 
